f097ef747d0f2880b1e303e66fd29f69c70d6848
Nikola Milovanovic <nikolla.millovanovic@gmail.com>
2022-09-05 23:22:46 +0200

    Import text of the exercises for lecture

--- src/p05_09_2022/Zadatak3.java
+++ src/p05_09_2022/Zadatak3.java
@@ -3,6 +3,28 @@ package p05_09_2022;
 import java.util.ArrayList;
 
 public class Zadatak3 {
+//	 Zadatak
+//	 Kreirati abstraktnu klasu Figura koja ima
+//	 abstraktnu metodu povrsina
+//	 abstraktnu metodu obim
+//	 metodu koja stampa podatke u formatu:
+//	 Povrsina je (povrsina)
+//	 Obim je (obim)
+//
+//	 Kreirati klasu JednakostranicniTrougao koja nasledjuje klasu Figura:
+//	 stranicu a
+//	 gettere/settere/konstruktore
+//	 implementira metodu povrsina po formuli (a * a) * 1.73205 / 4
+//	 implementira metodu obim
+//
+//	 Kreirati klasu Pravougaonik koja nasledjuje klasu Figura:
+//	 stranice a i b
+//	 gettere/settere/konstruktore
+//	 implementira metodu povrsina
+//	 implementira metodu obim
+//
+//	 U glavnoj klasi kreirati niz od 5 figura (2 trougla, 3 pravougaonika) zatim sabrati sve povrsine i obime i za svaku figuru ispisati ponaosob
+
 
 	public static void main(String[] args) {
 		ArrayList<Figura> figure = new ArrayList<Figura>();
--- src/p05_09_2022/Zadatak5.java
+++ src/p05_09_2022/Zadatak5.java
@@ -3,6 +3,42 @@ package p05_09_2022;
 public class Zadatak5 {
 
 	public static void main(String[] args) {
+//		Zadatak(ZA VEZBANJE)
+//		Caribic zeli da napravi program koji omogucava da se kreira proizvod sa proizvoljnim brojem podataka
+//		Npr: Ice point vanila sa dodacima: plazma, cokolada
+//		Npr: Pica sa dodacima: kackavalj, masline, kecap, majonez
+//
+//		Kreirati klasu Dodatak ili Sastojak kako hocete koja ima:
+//		 naziv dodatka
+//		 cenu dodatka
+//		 konstruktore, gettere i settere koji su vam potrebni
+//		 metodu stampaj koja stampa u formatu:
+//		(dodatak)  (cena)
+//
+//		Kreirati abstraktnu klasu Proizvod koja ima:
+//		 niz dodataka
+//		 metodu ubaciDodatak koja dodaje dodatak u niz
+//		 metodu koja vraca cenu svih dodataka koje ima proizvod
+//		 abstraktnu metodu ukupnu racunaj cenu 
+//		 abstraktnu metodu stampaj
+//
+//
+//
+//		Kreirati klasu IcePoint koja nasledjuje klasu Proizvod i koja ima:
+//		 tip sladoleda (vanila, cokolada)
+//		 atribut koji kaze da li je mali ice point ili veliki (promenljiva tipa boolean)
+//		 override uje metodu koja ukupnu cenu racuna:
+//		ako je mali ice point na cenu svih dodataka doda 100
+//		ako je veliki ice point cenu svih dodataka doda 130
+//		override uje metodu stampaj da stampa sve podatke
+//			
+//		Kreirati klasu Pica koja nasledjuje klasu Proizvod i koja ima:
+//		 cena podloge
+//		 override uje metodu koja ukupnu racuna cenu tako sto na cenu svih dodataka doda i cenu podloge
+//		override uje metodu stampaj da stampa sve podatke
+//			
+//		U glavnom programu kreirati niz proizvoda od 3 IcePinta i 2 Pice. Svakom proizvodu dodati po nekoliko dodataka i na kraju ispisati ukupnu cenu porudzbine.
+
 		Dodatak d1 = new Dodatak("plazma", 20);
 		Dodatak d2 = new Dodatak("cokolada", 40);
 		Dodatak d3 = new Dodatak("pistaci", 50);
